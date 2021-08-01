7e8e252079691c1d1021cde8ce73fc29271dc232
iuriiGav <iurii_gavryliuk@ukr.net>
2021-08-01 16:12:32 +0300

    changed hero festival dates field, now if empty, field won't show

--- template-parts/homepage/section-hero.php
+++ template-parts/homepage/section-hero.php
@@ -1,12 +1,14 @@
-<div class="current-festival-dates">
+<?php if (!empty(get_field('homepage_current_festival_edition_heading')) || !empty(get_field('homepage_current_festival_dates'))) : ?>
+    <div class="current-festival-dates">
 
-<h3 class="current-festival-dates__heading">
-<?php esc_html_e(get_field('homepage_current_festival_edition_heading'), 'saulkrasti-jazz-festival') ?>
+        <h3 class="current-festival-dates__heading">
+            <?php esc_html_e(get_field('homepage_current_festival_edition_heading'), 'saulkrasti-jazz-festival') ?>
 
-</h3>
-<h1 class="ig_tc-brand current-festival-dates__dates">
-<?php esc_html_e(get_field('homepage_current_festival_dates'), 'saulkrasti-jazz-festival') ?>
+        </h3>
+        <h1 class="ig_tc-brand current-festival-dates__dates">
+            <?php esc_html_e(get_field('homepage_current_festival_dates'), 'saulkrasti-jazz-festival') ?>
 
-</h1>
-</div>
+        </h1>
+    </div>
 
+<?php endif; ?>
