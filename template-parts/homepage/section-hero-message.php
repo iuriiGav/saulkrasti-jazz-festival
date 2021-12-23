d5053a0579471cbc65171c7473971d24d7a4efce
iuriiGav <iurii_gavryliuk@ukr.net>
2021-12-23 23:19:07 +0200

    added christmas font to typography and christmas-font style to section-hero-message

--- template-parts/homepage/section-hero-message.php
+++ template-parts/homepage/section-hero-message.php
@@ -1,6 +1,6 @@
 <?php
 if (!empty(get_field('homepage_hero__important_message'))) :?>
-<div class="hero__important-message">
+<div class="hero__important-message hero__important-message--christmas-font">
 
     <?php echo get_field('homepage_hero__important_message') ?>
 
